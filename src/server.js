19cca87f430a64cfe54c31b0879a8674845ebbb2
shaharKamay <shahar.kamay@gmail.com>
2021-11-07 18:12:01 +0200

    intalled morgan and display log info to the console.log

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 const apiRouter = require('./routes/api.js')
 const infoRouter = require('./routes/info.js')
 
@@ -8,8 +9,12 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
-app.use('/api', apiRouter)
+app.use('/api', apiRouter);
 app.use('/info', infoRouter);
 
 // start the server
