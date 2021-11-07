6d50eaab0121c368079d29232dcbe3607d10b630
shaharKamay <shahar.kamay@gmail.com>
2021-11-07 14:49:16 +0200

    added get info request that response with phonebook1.json data.length and the dateTime that the request was sent

--- src/server.js
+++ src/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const fs = require('fs');
 const apiRouter = require('./routes/api.js')
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.get('/info', (req, res, next) => {
+    const date = new Date().toLocaleString('en-US');
+    const path = `./src/database/phonebooks/phonebook1.json`;
+    const stringData = fs.readFileSync(path, 'utf-8');
+    const data = JSON.parse(stringData);
+
+    res.json(`Phonebook has info for ${data.length} people\n${date} (United States Standard Time)`);
+    res.end();
+
+})
 
 app.use('/api', apiRouter)
 
