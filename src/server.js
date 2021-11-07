e0b06a83149e5102ba880d6c2b3b9ffa770df2bf
shaharKamay <shahar.kamay@gmail.com>
2021-11-07 18:15:57 +0200

    finished excercises 3.1-3.6

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const morgan = require('morgan');
 const apiRouter = require('./routes/api.js')
 const infoRouter = require('./routes/info.js')
 
@@ -7,12 +6,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-
-morgan.token("body", (req, res) => JSON.stringify(req.body));
-app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
-);
 
 app.use('/api', apiRouter);
 app.use('/info', infoRouter);
